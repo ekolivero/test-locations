482151e92828bf57efa0f382fdc996acd6c8aff3
Stefano Rota <stefano.rota@jobtome.com>
2024-07-09 15:51:58 +0200

    Autocomplete endpoint

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI, Query, HTTPException
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import NotFoundError, RequestError, ConnectionError, TransportError
-
+import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-es = Elasticsearch(hosts=["https://c9778aaeafc64b90a4d6ac93c7a07a46.eu-west-1.aws.found.io:443"], api_key="bG5NSGw1QUI4RmI3c1BVbWoyaUk6MHB2c2MycW9TTzZiOE9haUhIRk56dw==")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#es = Elasticsearch(hosts=["http://localhost:9200"])
+es = Elasticsearch(hosts=[os.getenv('ELASTIC_HOST')], api_key=os.getenv('ELASTIC_API_KEY'))
+print('Elastic ping:', es.ping())
 index_name = "locations"
 
 @app.get("/suggest/")
 async def suggest(query: str = Query(..., min_length=3)):
-    
+
     try:
+        
+        """
         query = {
             "suggest": {
                 "suggestions": {
@@ -29,6 +43,32 @@ async def suggest(query: str = Query(..., min_length=3)):
         if "suggest" in response:
             options = response["suggest"]["suggestions"][0]["options"]
             suggestions = [opt["_source"]["label"] for opt in options]
+        """
+
+        query = {
+            "query": {
+                "multi_match": {
+                    "query": query,
+                    "type": "bool_prefix",    
+                    "fields": ["suggest"]
+                }
+            },
+            "size": 20,
+            "sort": [{"sorting":{"order":"asc"}}, {"area": {"order":"desc"}}],
+            "_source": ["id", "label","level","parents"]
+        }
+
+        response = es.search(index=index_name, body=query)
+
+        suggestions = []
+        for r in response['hits']['hits']:
+            res = {
+                'id': r['_source']['id'],
+                'label': r['_source']['label'],
+                'level': r['_source']['level'],
+                'parents': r['_source']['parents'][0:-1]
+            }
+            suggestions.append(res)
 
         return {"suggestions": suggestions}
 
