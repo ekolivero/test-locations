341feba003ce5b13b53e0b55c3d5c2f1ca5f0041
Stefano Rota <stefano.rota@jobtome.com>
2024-07-11 18:57:45 +0200

    new endpoints

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ app.add_middleware(
 es = Elasticsearch(hosts=[os.getenv('ELASTIC_HOST')], api_key=os.getenv('ELASTIC_API_KEY'))
 print('Elastic ping:', es.ping())
 
+level_mapping = {0:'region', 1:'province', 2:'city', 3:'district', 4:'neighborhood'}
+
 
 @app.get("/suggest/")
 async def suggest(query: str = Query(..., min_length=3)):
@@ -37,7 +39,7 @@ async def suggest(query: str = Query(..., min_length=3)):
             },
             "size": 20, 
             "sort": [{"sorting":{"order":"asc"}}, {"area": {"order":"desc"}}],
-            "_source": ["id", "label", "level", "parents"] 
+            "_source": ["id", "label", "level", "page", "parents"] 
         }
 
         response = es.search(index=index_name, body=q)
@@ -48,6 +50,7 @@ async def suggest(query: str = Query(..., min_length=3)):
                 'id': r['_source']['id'],
                 'label': r['_source']['label'],
                 'level': r['_source']['level'],
+                'page': r['_source']['page'],
                 'parents': r['_source']['parents'][0:-1]
             }
             suggestions.append(res)
@@ -114,11 +117,95 @@ async def geocoding(latitude: float = Query(..., description="Latitude of the po
 
 
 
-@app.get("/houses/")
-async def houses(level: str = Query(..., description="The level of the location hierarchy [region, province, city, district, neighborhood]"),
-                 idx: str = Query(..., description="The id of the location")):
+@app.get("/page/")
+async def page(query: str = Query(...)):
 
-    index_name = 'houses'
+    try:
+        query = {
+            "query": {
+                "term": {
+                    "page": query
+                }
+            },
+            "sort": [
+                {"sorting": {"order": "asc"}} 
+            ],
+            "size": 1,
+            "_source": ["id", "level", "label"]
+        }
+
+        response = es.search(index=index_name, body=query)
+
+        pages = []
+        for r in response['hits']['hits']:
+            res = {
+                'id': r['_source']['id'],
+                'label': r['_source']['label'],
+                'level': r['_source']['level']
+            }
+            pages.append(res)
+
+        return {"pages": pages}
+
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail="Resource not found")
+
+    except RequestError as e:
+        raise HTTPException(status_code=400, detail="Bad request")
+
+    except ConnectionError as e:
+        raise HTTPException(status_code=503, detail="Elasticsearch connection error")
+
+    except TransportError as e:
+        raise HTTPException(status_code=503, detail="Elasticsearch transport error")
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+
+
+@app.get("/houses/")
+#async def houses(level: str = Query(..., description="The level of the location hierarchy [region, province, city, district, neighborhood]"),
+#                 idx: str = Query(..., description="The id of the location")):
+
+async def houses(page: str = Query(...)):
+    try:
+
+        query = {
+            "query": {
+                "term": {
+                    "page": page
+                }
+            },
+            "sort": [
+                {"sorting": {"order": "asc"}} 
+            ],
+            "size": 1,
+            "_source": ["id", "level", "label"]
+        }
+
+        response = es.search(index='locations', body=query)
+
+        idx = response['hits']['hits'][0]['_source']['id']
+
+        level = level_mapping[response['hits']['hits'][0]['_source']['level']]
+
+    except NotFoundError as e:
+        raise HTTPException(status_code=404, detail="Resource not found")
+
+    except RequestError as e:
+        raise HTTPException(status_code=400, detail="Bad request")
+
+    except ConnectionError as e:
+        raise HTTPException(status_code=503, detail="Elasticsearch connection error")
+
+    except TransportError as e:
+        raise HTTPException(status_code=503, detail="Elasticsearch transport error")
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Internal server error")
     
     try:
         
@@ -153,7 +240,6 @@ async def houses(level: str = Query(..., description="The level of the location 
 
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
-
 
 
 if __name__ == '__main__':
